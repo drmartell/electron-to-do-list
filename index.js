7421cf23f403b75765a127a4b141842c4dd9541d
drmartell <drmartell@gmail.com>
2020-03-19 15:42:34 -0700

    add ability to add to-do and clear todos

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const electron = require('electron');
 
-const { app, BrowserWindow, Menu } = electron;
+const { app, BrowserWindow, Menu, ipcMain } = electron;
 
 let mainWindow;
 let addWindow;
@@ -12,11 +12,16 @@ app.on('ready', () => {
     }
   });
   mainWindow.loadURL(`file://${__dirname}/main.html`);
+  mainWindow.on('closed', () => app.quit());
 
   const mainMenu = Menu.buildFromTemplate(menuTemplate);
   Menu.setApplicationMenu(mainMenu);
 });
 
+ipcMain.on('todo:add', (_e, todo) => {
+  mainWindow.webContents.send('todo:add', todo);
+  addWindow.close();
+});
 
 const createAddWindow = () => {
   addWindow = new BrowserWindow({
@@ -28,7 +33,12 @@ const createAddWindow = () => {
     title: 'Add New Todo'
   });
   addWindow.loadURL(`file://${__dirname}/add.html`);
-}
+  addWindow.on('closed', () => addWindow = null); // allow for garbage collection
+};
+
+const clearTodos = () => {
+  mainWindow.webContents.send('todos:clear');
+};
 
 const menuTemplate = [{
   label: 'File',
@@ -36,12 +46,30 @@ const menuTemplate = [{
     label: 'New To-Do',
       click() { createAddWindow() }
     }, {
+    label: 'Clear',
+      click() { clearTodos() }
+    }, {
     label: 'Quit',
-      accelerator: process.platform === 'darwin' ? 'Command+Q' : 'Ctrl+Q',
-      click() {
-        app.quit();
-      }
+    click() {
+      app.quit();
+    },
+    accelerator: process.platform === 'darwin' ? 'Command+Q' : 'Ctrl+Q',
   }]
 }];
 
 if (process.platform === 'darwin') menuTemplate.unshift({});
+
+if(process.env.NODE_ENV !== 'production') {
+  menuTemplate.push({
+    label: 'View',
+    submenu: [{
+      role: 'reload' // one of Electrons several built-in menu options
+    }, {
+      label: 'Toggle Developer Tools',
+      click(_item, focusedWindow) {
+        focusedWindow.toggleDevTools();
+      },
+      accelerator: process.platform === 'darwin' ? 'Command+Alt+I' : 'Ctrl+Shift+I',
+    }]
+  })
+};
